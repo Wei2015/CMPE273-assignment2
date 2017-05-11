641541ae49fea37dbe63f38702446f9d74672ba3
Wei Yao <wei.yao@sjsu.edu>
2017-05-11 09:05:48 -0700

    remove one file not in use

--- putOrderID.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-def handler(event, context):
-    dynamodb = boto3.resource('dynamodb')
-    orderTable = dynamodb.Table('order')
-    table.update_item(
-        Key={'order_id': event['order_id']},
-        UpdateExpression: "ADD a
